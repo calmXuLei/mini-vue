bf7cd4ccbbc1b85ba115d9b4468eb6b79a9e31b1
xl <xl18845898715@gmail.com>
2024-04-06 21:58:59 +0800

    reconstruct: 抽离 createActiveObject

--- src/reactivity/reactive.ts
+++ src/reactivity/reactive.ts
@@ -2,9 +2,14 @@ import { mutableHandlers, readonlyHandlers } from "./baseHandler";
 
 
 export function reactive(raw) {
-  return new Proxy(raw, mutableHandlers);
+  return createActiveObject(raw, mutableHandlers);
 }
 
 export function readonly(raw) {
-  return new Proxy(raw, readonlyHandlers)
+  return createActiveObject(raw, readonlyHandlers)
+}
+
+// 使用函数封装，增强语义化
+function createActiveObject(raw, baseHandlers) {
+  return new Proxy(raw, baseHandlers);
 }
