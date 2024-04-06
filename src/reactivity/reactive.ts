06004d44b204a49a627f32312ad2cef6aa61ad00
xl <xl18845898715@gmail.com>
2024-04-06 21:47:04 +0800

    reconstructing: 抽离 getter、setter

--- src/reactivity/reactive.ts
+++ src/reactivity/reactive.ts
@@ -1,35 +1,36 @@
 import { track, trigger } from "./effect";
 
-export function reactive(raw) {
-
-  return new Proxy(raw, {
-
-    get(target, key) {
-      const res = Reflect.get(target, key);
-
+function createGetter(isReadonly = false) {
+  return function get(target, key) {
+    const res = Reflect.get(target, key);
+    if (!isReadonly) {
       track(target, key);
-      return res;
-    },
-
-    set(target, key, value) {
-      const res = Reflect.set(target, key, value);
-
-      trigger(target, key);
-      return res;
     }
+    return res;
+  }
+}
+
+// 程序结构的相对性
+function createSetter() {
+  return function set(target, key, value) {
+    const res = Reflect.set(target, key, value);
+
+    trigger(target, key);
+    return res;
+  }
+}
+
+export function reactive(raw) {
+  return new Proxy(raw, {
+    get: createGetter(),
+    set: createSetter()
   })
 
 }
 
 export function readonly(raw) {
   return new Proxy(raw, {
-
-    get(target, key) {
-      const res = Reflect.get(target, key);
-      // readonly 不需要收集依赖
-      return res;
-    },
-
+    get: createGetter(true),
     set(target, key, value) {
       // TODO: 报错 
       return true;
